68ae675d8c86dcf029f70a613056b6ccf7777c13
Yongseok Oh <yongseok.oh@sk.com>
2017-01-04 08:13:25 +0900

    Perf script with varied block size and qdepth

--- examples/perf/script.sh
+++ examples/perf/script.sh
@@ -1,7 +1,41 @@
 #!/bin/sh
 
-echo "10G file size, 4K block size, libaio, 128 qdepth, random workload, direct I/O, write workload"
-sudo ./perf -t spdk -d 01:00 -f -m -S 10240 -B 4096 -E libaio -Q 128 -R -D -W
+file_size=128 #giga
+for qdepth in 1 2 4 8 16 32 64 128 256
+do
+	echo "${file_size}G file size, 4K block size, libaio, $qdepth qdepth, random workload, direct I/O, write workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B 4096 -E libaio -Q $qdepth -R -D -W > 4K_write_${qdepth}_qdepth_${file_size}G_random.log
 
-echo "10G file size, 4K block size, libaio, 128 qdepth, random workload, direct I/O, read workload"
-sudo ./perf -t spdk -d 01:00 -f -m -S 10240 -B 4096 -E libaio -Q 128 -R -D
+	echo "${file_size}G file size, 4K block size, libaio, $qdepth qdepth, random workload, direct I/O, read workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B 4096 -E libaio -Q $qdepth -R -D > 4K_read_${qdepth}_qdepth_${file_size}G_random.log
+done
+
+file_size=128 #giga
+for qdepth in 1 2 4 8 16 32 64 128 256
+do
+	echo "${file_size}G file size, 128K block size, libaio, $qdepth qdepth, random workload, direct I/O, write workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B $((128*1024)) -E libaio -Q $qdepth -R -D -W > 128K_write_${qdepth}_qdepth_${file_size}G_random.log
+
+	echo "${file_size}G file size, 128K block size, libaio, $qdepth qdepth, random workload, direct I/O, read workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B $((128*1024)) -E libaio -Q $qdepth -R -D > 128K_read_${qdepth}_qdepth_${file_size}G_random.log
+done
+
+file_size=128 #giga
+for qdepth in 1 2 4 8 16 32 64 128 256
+do
+	echo "${file_size}G file size, 4K block size, libaio, $qdepth qdepth, seq workload, direct I/O, write workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B 4096 -E libaio -Q $qdepth -D -W > 4K_write_${qdepth}_qdepth_${file_size}G_seq.log
+
+	echo "${file_size}G file size, 4K block size, libaio, $qdepth qdepth, seq workload, direct I/O, read workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B 4096 -E libaio -Q $qdepth -D > 4K_read_${qdepth}_qdepth_${file_size}G_seq.log
+done
+
+file_size=128 #giga
+for qdepth in 1 2 4 8 16 32 64 128 256
+do
+	echo "${file_size}G file size, 128K block size, libaio, $qdepth qdepth, seq workload, direct I/O, write workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B $((128*1024)) -E libaio -Q $qdepth -D -W > 128K_write_${qdepth}_qdepth_${file_size}G_seq.log
+
+	echo "${file_size}G file size, 128K block size, libaio, $qdepth qdepth, seq workload, direct I/O, read workload"
+	sudo ./perf -t spdk -d 01:00 -f -m -S $((file_size*1024)) -B $((128*1024)) -E libaio -Q $qdepth -D > 128K_read_${qdepth}_qdepth_${file_size}G_seq.log
+done
