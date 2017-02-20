cade9a8c60b3533a42764eccbffb12db72de4116
SonJuHyung <tooson9010@gmail.com>
2017-02-21 04:19:16 +0900

    cpu_info

--- examples/kernel_filesystem/kernel_raw_output_parse.sh
+++ examples/kernel_filesystem/kernel_raw_output_parse.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 FIO_MINIMAL_DECODER=~/fio_minimal_decoder/fio_minimal_output_decode.py
-OUTPUT_PATH=output
-PREFIXS=(kernel_ext2 kernel_xfs)
+OUTPUT_PATH=output 
+ARRANGE_PATH=arrange
+PREFIXS=(kernel_ext2)
+PREFIX2=async
+#kernel_xfs)
 
 for PREFIX in "${PREFIXS[@]}"
 do
 
-    READ_IOPS_LOG=${OUTPUT_PATH}/${PREFIX}_read_iops.log
-    WRITE_IOPS_LOG=${OUTPUT_PATH}/${PREFIX}_write_iops.log
+    READ_IOPS_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_read_iops.log
+    WRITE_IOPS_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_write_iops.log
 
-    READ_LAT_AVG_LOG=${OUTPUT_PATH}/${PREFIX}_read_lat_avg.log
-    WRITE_LAT_AVG_LOG=${OUTPUT_PATH}/${PREFIX}_write_lat_avg.log
+    READ_LAT_AVG_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_read_lat_avg.log
+    WRITE_LAT_AVG_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_write_lat_avg.log
 
-    READ_LAT_MIN_LOG=${OUTPUT_PATH}/${PREFIX}_read_lat_min.log
-    WRITE_LAT_MIN_LOG=${OUTPUT_PATH}/${PREFIX}_write_lat_min.log
+    READ_LAT_MIN_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_read_lat_min.log
+    WRITE_LAT_MIN_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_write_lat_min.log
 
-    READ_LAT_MAX_LOG=${OUTPUT_PATH}/${PREFIX}_read_lat_max.log
-    WRITE_LAT_MAX_LOG=${OUTPUT_PATH}/${PREFIX}_write_lat_max.log
+    READ_LAT_MAX_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_read_lat_max.log
+    WRITE_LAT_MAX_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_write_lat_max.log
+
+    READ_CPU_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_read_cpu.log
+    WRITE_CPU_LOG=${OUTPUT_PATH}/${ARRANGE_PATH}/${PREFIX}_${PREFIX2}_write_cpu.log
 
     rm $READ_IOPS_LOG
     rm $WRITE_IOPS_LOG
@@ -29,67 +35,87 @@ do
     rm $WRITE_LAT_MIN_LOG
 
     rm $READ_LAT_MAX_LOG
-    rm $WRITE_LAT_MAX_LOG
+    rm $WRITE_LAT_MAX_LOG 
 
-#for workload in read randread
+    rm $READ_CPU_LOG
+    rm $WRITE_CPU_LOG
+
+    #for workload in read randread
     for workload in randread randwrite
     do
-	for numjobs in {1..6} 
-	do
-		for qdepth in 1 2 4 8 16 32 64 128 256
-		do
-		    if [ $workload = read -o $workload = write ] ; then 
-			block_size=$((64*1024))
-		    else
-			block_size=$((4096))
-		    fi
+        for numjobs in {1..6} 
+        do
+            for qdepth in 1 2 4 8 16 32 64 128 256
+            do
+                if [ $workload = read -o $workload = write  ] ; then 
+                    block_size=$((64*1024))
+                else
+                    block_size=$((4096))
+                fi
 
-		    output=${OUTPUT_PATH}/${PREFIX}_async_numjobs_${numjobs}_q_${qdepth}_block_${block_size}_workload_${workload}.log
-		    if [ $workload = randread ] ; then 
+                output=${OUTPUT_PATH}/${PREFIX}_${PREFIX2}_numjobs_${numjobs}_q_${qdepth}_block_${block_size}_workload_${workload}.log
+                if [ $workload = randread  ] ; then 
 
-			# IOPS
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_iops | head -n 1)
-			echo $ret
-			echo qd $qdepth  $ret >> ${READ_IOPS_LOG}
+                    # IOPS
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_iops | head -n 1)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${READ_IOPS_LOG}
 
-			# AVG LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_mean)
-			echo $ret
-			echo qd $qdepth  $ret >> ${READ_LAT_AVG_LOG}
+                    # AVG LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_mean)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${READ_LAT_AVG_LOG}
 
-			# MAX LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_max)
-			echo $ret
-			echo qd $qdepth  $ret >> ${READ_LAT_MAX_LOG}
+                    # MAX LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_max)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${READ_LAT_MAX_LOG}
 
-			# MIN LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_min)
-			echo $ret
-			echo qd $qdepth  $ret >> ${READ_LAT_MIN_LOG}
-		    else
+                    # MIN LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep read_lat_min)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${READ_LAT_MIN_LOG}
 
-			# IOPS
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_iops | head -n 1)
-			echo $ret
-			echo qd $qdepth  $ret >> ${WRITE_IOPS_LOG}
 
-			# AVG LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_mean)
-			echo $ret
-			echo qd $qdepth  $ret >> ${WRITE_LAT_AVG_LOG}
+                    ret1=$($FIO_MINIMAL_DECODER $output | grep cpu_user | awk '{print "cpu_usr" "\t"$3}')
+                    ret2=$($FIO_MINIMAL_DECODER $output | grep cpu_sys | awk '{print "cpu_sys" "\t"$3}')
+                    ret3=$($FIO_MINIMAL_DECODER $output | grep cpu_csw | awk '{print "cpu_csw" "\t"$3}')
+                    ret4=$($FIO_MINIMAL_DECODER $output | grep cpu_mjf | awk '{print "cpu_majf" "\t"$3}')
+                    ret5=$($FIO_MINIMAL_DECODER $output | grep pu_minf | awk '{print "cpu_minf" "\t"$3}')
+                    echo $ret1 $ret2 $ret3 $ret4 $ret5
+                    echo qd $qdepth thread $numjobs  $ret1 $ret2 $ret3 $ret4 $ret5 >> ${READ_CPU_LOG}
+                else
 
-			# MAX LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_max)
-			echo $ret
-			echo qd $qdepth  $ret >> ${WRITE_LAT_MAX_LOG}
+                    # IOPS
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_iops | head -n 1)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${WRITE_IOPS_LOG}
 
-			# MIN LAT 
-			ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_min)
-			echo $ret
-			echo qd $qdepth  $ret >> ${WRITE_LAT_MIN_LOG}
-		    fi
-		done
-	done
+                    # AVG LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_mean)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${WRITE_LAT_AVG_LOG}
+
+                    # MAX LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_max)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${WRITE_LAT_MAX_LOG}
+
+                    # MIN LAT 
+                    ret=$($FIO_MINIMAL_DECODER $output | awk '{print $2"\t"$3}' | grep write_lat_min)
+                    echo $ret
+                    echo qd $qdepth  $ret >> ${WRITE_LAT_MIN_LOG}
+
+                    ret1=$($FIO_MINIMAL_DECODER $output | grep cpu_user | awk '{print "cpu_usr" "\t"$3}')
+                    ret2=$($FIO_MINIMAL_DECODER $output | grep cpu_sys | awk '{print "cpu_sys" "\t"$3}')
+                    ret3=$($FIO_MINIMAL_DECODER $output | grep cpu_csw | awk '{print "cpu_csw" "\t"$3}')
+                    ret4=$($FIO_MINIMAL_DECODER $output | grep cpu_mjf | awk '{print "cpu_majf" "\t"$3}')
+                    ret5=$($FIO_MINIMAL_DECODER $output | grep pu_minf | awk '{print "cpu_minf" "\t"$3}')
+                    echo $ret1 $ret2 $ret3 $ret4 $ret5
+                    echo qd $qdepth thread $numjobs  $ret1 $ret2 $ret3 $ret4 $ret5 >> ${WRITE_CPU_LOG}
+                fi
+            done
+        done
     done
 
     cat $READ_IOPS_LOG
--- examples/perf/perf_kernel/experiment.sh
+++ examples/perf/perf_kernel/experiment.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-
-WORKING_DIR=/home/son/nvfuse/nvfuse/examples/perf/perf_kernel
-MOUNT_DIR=$WORKING_DIR/mnt
+NVFUSE_DIR=/home/son/nvfuse/nvfuse/
 DEV=/dev/nvme0n1
-PGM=$WORKING_DIR/kernel_fs
-RESULT_DIR=$WORKING_DIR/result
-RESULT_DIR_FIO=$RESULT_DIR/FIO 
-RESULT_DIR_META=$RESULT_DIR/META
 
-experiment_type=$1
+WORKING_DIR=$(pwd)
+PGM=$WORKING_DIR/kernel_fs
+MOUNT_DIR=$WORKING_DIR/mnt
+RESULT_DIR=$WORKING_DIR/result
+RESULT_DIR_FIO=$RESULT_DIR/fio
+RESULT_DIR_META=$RESULT_DIR/meta
 
 file_systems[0]='ext2'
 file_systems[1]='ext4'
 file_systems[2]='xfs'
 
+experiment_type=$1
+
+## meta experiment
+COUNT=10000000
+OP_TYPE="total"
+
 function meta_experiment(){
-    local fs=$1
+   
     for i in {1..6}
     do
         echo start $i
         echo 3 > /proc/sys/vm/drop_caches
-        ./kernel_fs -M total -C 10 -T $i > $RESULT_DIR_META/${file_systems[$fs]}_perf_th_$i.txt
+        ./kernel_fs -M $OP_TYPE -C $COUNT -T $i > $RESULT_DIR_META/${file_systems[$1]}_perf_th_$i.txt
         echo end $i
         echo ""
     done
@@ -29,16 +34,15 @@ function meta_experiment(){
 function fio_experiment(){
 
     # 4k random write
-    local fs=$1
     for i in {1..6}
     do
-        fio --name=sync --directory=. --ioengine=sync --size=32g --bs=4k --fsync=1 --rw=randwrite --thread=1 --numjobs=${i} --group_reporting --output=$RESULT_DIR_FIO/${file_systems[fs]}_4kb_syncio_fsync_write_th_${i}.txt
+    ./kernel_fs fio --name=sync --directory=. --ioengine=sync --size=32g --bs=4k --fsync=1 --rw=randwrite --thread=1 --numjobs=${i} --group_reporting --output=$RESULT_DIR_FIO/${file_systems[$1]}_4kb_syncio_fsync_write_th_${i}.txt
     done
 
     # 4k random read
     for i in {1..6}
     do
-        fio --name=sync --directory=. --ioengine=sync --size=32g --bs=4k -rw=randread --thread=1 --numjobs=${i} --group_reporting --output=$RESULT_DIR_FIO/${file_system[fs]}_kb_syncio_fsync_read_th_${i}.txt
+        fio --name=sync --directory=. --ioengine=sync --size=32g --bs=4k -rw=randread --thread=1 --numjobs=${i} --group_reporting --output=$RESULT_DIR_FIO/${file_system[$1]}_kb_syncio_fsync_read_th_${i}.txt
     done
 }
 
@@ -63,49 +67,54 @@ function init(){
         mkdir $RESULT_DIR_META
     fi 
 
-    source /home/son/nvfuse/nvfuse/scripts/setup.sh reset
+    source $NVFUSE_DIR/scripts/setup.sh reset
 
     echo $((4*1024*1024)) > /proc/sys/vm/dirty_bytes
     echo $((4*1024*1024)) > /proc/sys/vm/dirty_background_bytes 
+
+    if cat /proc/mounts | grep -F $MOUNT_DIR > /dev/null
+    then 
+        umount $MOUNT_DIR
+    fi
 }
 
 function destroy(){
     echo 0 > /proc/sys/vm/dirty_bytes
     echo 0 > /proc/sys/vm/dirty_background_bytes 
+    
+    rm -rf $MOUNT_DIR
 }
 
 init
+
 for i in {0..2}
-do 
+do     
     if [ ${file_systems[$i]} = "xfs" ]
     then
         mkfs.${file_systems[$i]} -f $DEV  
     else
         yes | mkfs.${file_systems[$i]} $DEV
-    fi 
+    fi   
+        
     mount $DEV $MOUNT_DIR
-
     cp $PGM $MOUNT_DIR        
     cd $MOUNT_DIR
     if [ $experiment_type = "meta" ]
     then
-        meta_experiment i
+        meta_experiment $i
     elif [ $experiment_type = "fio" ]
     then
-        fio_experiment i
+        fio_experiment $i
     else
         echo "type missing"
     fi
     cd $WORKING_DIR 
     umount $MOUNT_DIR
+
 done
 destroy
     
-#cp $PGM $MOUNT_DIR
-#yes | mkfs.xfs $DEV
-#umount $MOUNT_DIR 
 
-#ext4 
 
 
 
